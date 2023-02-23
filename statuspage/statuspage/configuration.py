7cea3c5bf1cd0dc7d27e0513fa7e7e7089014ecd
guyguyr23 <62642694+guyguyr23@users.noreply.github.com>
2023-02-23 10:25:45 +0200

    cahnge the redis cluster

--- statuspage/statuspage/configuration.py
+++ statuspage/statuspage/configuration.py
@@ -23,14 +23,14 @@ DATABASE = {
 # for each. Full connection details are required.
 REDIS = {
     'tasks': {
-        'HOST': 'replica.final-project.eqy8yf.usw1.cache.amazonaws.com',      # Redis server
+        'HOST': 'master.final-project.eqy8yf.usw1.cache.amazonaws.com',      # Redis server
         'PORT': 6379,             # Redis port         
         'PASSWORD': '',           # Redis password (optional)
         'DATABASE': 0,            # Database ID
         'SSL': False,             # Use SSL (optional)
     },
     'caching': {
-        'HOST': 'replica.final-project.eqy8yf.usw1.cache.amazonaws.com',
+        'HOST': 'master.final-project.eqy8yf.usw1.cache.amazonaws.com',
         'PORT': 6379,
         'PASSWORD': '',
         'DATABASE': 1,            # Unique ID for second database
